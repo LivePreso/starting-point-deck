e0780de1cc6f500210fefbb7ae14b6644de3b573
Lauren Rui <lauren.rui@salespreso.com>
2020-09-02 13:27:11 +1000

    feat(fieldsets): Save basic customer and attendee info, import lodash

--- src/js/hooks/fieldsets.js
+++ src/js/hooks/fieldsets.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Requests from './requests.js';
 
 export default function(options) {
@@ -7,7 +8,26 @@ export default function(options) {
     fields: [],
 
     onSave: function({ data }) {
-      return { data };
+      const customerData = _.get(data, 'customers.0', {});
+
+      const customer = {
+        title: customerData.title,
+        logo_original: customerData.logo_original
+      };
+
+      const attendees = _.map(data.contacts, function(contact) {
+        return {
+          full_name: contact.fullName,
+          first_name: contact.name_first,
+          last_name: contact.name_last,
+          email: contact.email,
+          phone: contact.phone
+        };
+      });
+
+      const context = { attendees, customer };
+
+      return { context };
     }
   });
 }
