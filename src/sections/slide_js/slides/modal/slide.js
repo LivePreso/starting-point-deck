79452275f8886ad407987bd28cab585474875753
Lauren Rui <lauren.rui@salespreso.com>
2021-02-16 17:26:14 +1100

    feat(deck): Add modal test slide

---
+++ src/sections/slide_js/slides/modal/slide.js
@@ -0,0 +1,36 @@
+new Slide({
+  onRendered: function() {
+    // TODO: Develop modal example
+    this.components.modal = new Modal(this, {
+      canCloseShroud: true,
+      content: [
+        new ModalContent(
+          `modal-content-1`,
+          this.utils.findEl('.js-modal-content-1')
+        )
+      ]
+    });
+
+    // Source with compiled toggle title
+    this.components.source = new Source(
+      `${this.id}-source`,
+      this.utils.findEl('.js-source-container'),
+      {
+        textMap: [
+          {
+            key: 'Source',
+            text: 'Lots of words'
+          }
+        ]
+      }
+    );
+  },
+  onReady: function() {
+    this.modalButton = this.utils.findEl('.js-open-modal');
+    this.modalButton.addEventListener('click', e => {
+      this.components.modal.open();
+      this.components.modal.activeContent = 'modal-content-1';
+    });
+  },
+  onSubslideChange: function(index) {}
+});
