0b1e8dae198f0f2d009f22ac7a9665dabd3ba240
Lauren Rui <lauren.rui@livepreso.com>
2021-11-18 11:53:14 +1100

    feat(AMCharts): swap from importing to merging into deck.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,23 @@ var gulp = require('gulp');
 var deck_gulp = require('@salespreso/deck-gulp-tasks');
 
 deck_gulp(gulp, {
-    src: "src/",
-    dist: "dist/",
-    docs: true,
-    excludes: [
-      "js/external/amcharts4/*.js",
-      "js/external/amcharts4/**/*.js",
-      "sections/**/*.js",
-      "slide.js",
-      "templates/**/*.js"
-    ],
-    htmlExcludes: [
-      "partials/**.*",
-      "**/partials/**/*.*"
-    ],
-    autoprefixCss: true,
-    autoprefixerOptions: { grid: true },
-    logInjections: false,
-    nunjucks: true,
-    notifications: {
-      "error": true,
-      "success": true,
-      "sounds": true
-    }
+  src: 'src/',
+  dist: 'dist/',
+  docs: true,
+  excludes: ['sections/**/*.js', 'slide.js', 'templates/**/*.js'],
+  htmlExcludes: ['partials/**.*', '**/partials/**/*.*'],
+  babelIgnore: [
+    'src/js/components/00-1-core.js',
+    'src/js/components/00-2-charts.js',
+    'src/js/components/00-3-animated.js'
+  ],
+  autoprefixCss: true,
+  autoprefixerOptions: { grid: true },
+  logInjections: false,
+  nunjucks: true,
+  notifications: {
+    error: true,
+    success: true,
+    sounds: true
+  }
 });
